d033370d46349347626392a8fd4b024be39eebf3
Travis CI <travis@travis-ci.org>
2018-02-07 18:03:16 +0800

    docker

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# curl -sL https://raw.githubusercontent.com/gwuhaolin/adbot/master/setup.sh | sudo -E bash -
 
 # install nodejs 8
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
@@ -8,7 +9,7 @@ sudo apt-get install -y nodejs
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
 
 # get source
 git clone https://github.com/gwuhaolin/adbot.git
