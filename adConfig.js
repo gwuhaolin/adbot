0ebd4f5eaaaa12316b367e2918a8cc77b77f51df
Travis CI <travis@travis-ci.org>
2018-02-22 09:50:59 +0800

    docker

--- adConfig.js
+++ adConfig.js
@@ -1,34 +1,34 @@
 module.exports = {
-    google: {
-        iframeSelector: `.adsbygoogle iframe`,
-        prefixList: [
-            `https://www.googleadservices.com/pagead/aclk?`,
-            `http://www.googleadservices.com/pagead/aclk?`,
-            `https://adclick.g.doubleclick.net/pcs/click?`,
-            `http://adclick.g.doubleclick.net/pcs/click?`,
-            `https://adclick.g.doubleclick.net/aclk?`,
-            `http://adclick.g.doubleclick.net/aclk?`,
-            `https://googleads.g.doubleclick.net/aclk?`,
-            `http://googleads.g.doubleclick.net/aclk?`,
-            `/aclk?`
-        ],
-        urlReduce: (url) => {
-            if (url.startsWith('/aclk?')) {
-                return `https://googleads.g.doubleclick.net${url}`
-            }
-            return url;
-        }
-    },
-    baidu: {
-        iframeSelector: `iframe[id^='iframeu']`,
-        prefixList: [
-            `http://click.bes.baidu.com/adx.php?`,
-            `http://m.baidu.com/mobads.php?`,
-            `https://cpro.baidu.com/cpro/ui/uijs.php?`,
-            `http://m.baidu.com/mobads.php?`,
-            `https://m.baidu.com/mobads.php?`
-        ]
-    },
+    // google: {
+    //     iframeSelector: `.adsbygoogle iframe`,
+    //     prefixList: [
+    //         `https://www.googleadservices.com/pagead/aclk?`,
+    //         `http://www.googleadservices.com/pagead/aclk?`,
+    //         `https://adclick.g.doubleclick.net/pcs/click?`,
+    //         `http://adclick.g.doubleclick.net/pcs/click?`,
+    //         `https://adclick.g.doubleclick.net/aclk?`,
+    //         `http://adclick.g.doubleclick.net/aclk?`,
+    //         `https://googleads.g.doubleclick.net/aclk?`,
+    //         `http://googleads.g.doubleclick.net/aclk?`,
+    //         `/aclk?`
+    //     ],
+    //     urlReduce: (url) => {
+    //         if (url.startsWith('/aclk?')) {
+    //             return `https://googleads.g.doubleclick.net${url}`
+    //         }
+    //         return url;
+    //     }
+    // },
+    // baidu: {
+    //     iframeSelector: `iframe[id^='iframeu']`,
+    //     prefixList: [
+    //         `http://click.bes.baidu.com/adx.php?`,
+    //         `http://m.baidu.com/mobads.php?`,
+    //         `https://cpro.baidu.com/cpro/ui/uijs.php?`,
+    //         `http://m.baidu.com/mobads.php?`,
+    //         `https://m.baidu.com/mobads.php?`
+    //     ]
+    // },
     // jd: {
     //     iframeSelector: `iframe[src^='//u-x.jd.com/']`,
     //     prefixList: [
